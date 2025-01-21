55c321989c60b87a80403f08c654a129eb30f24c
Unfeel-coder <galex.ind@gmail.com>
2025-01-21 19:59:45 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,1 @@
-const form = document.getElementById('form');
-const results = document.getElementById('results');
 
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-
-  const formData = new FormData(form);
-  const data = Object.fromEntries(formData);
-
-  // Рассчитать сумму страхования
-  const coefficients = document.querySelectorAll('#coefficients ul li');
-  const remainingMortgageSum = data.remainingMortgageSum;
-  const mortgageCreditSum = data.mortgageCreditSum;
-  const termRepayment = data.termRepayment;
-  let remainingMortgageSumCoefficient = 0;
-  let termRepaymentCoefficient = 0;
-
-  // Определить коэффициент для оставшейся суммы кредита
-  if (remainingMortgageSum <= 1000000) {
